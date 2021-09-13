62297562fdf6cbb868c79408406c9ddbc87a70d4
Dyary Raoof Bayz <dyary2020@gmail.com>
2021-09-14 00:42:29 +0300

    Add remove book element from DOM functionality

--- script.js
+++ script.js
@@ -7,21 +7,37 @@ const form = document.querySelector('#form');
 const title = form.elements[0];
 const author = form.elements[1];
 const addBtn = document.querySelector('#addBtn');
-const removeBtns = document.querySelectorAll('.remove-button');
 const bookList = document.querySelector('#book-list');
 
+const removeBook = (i) => {
+  books = books.filter((a) => a !== books[i]);
+  console.log(i);
+  const bookElements = document.querySelectorAll('.book-element-wrapper');
+  bookList.removeChild(bookElements[i]);
+};
+
+const addEventToRemoveButtons = () => {
+  const removeBtns = document.querySelectorAll('.remove-button');
+  for (let i = 0; i < removeBtns.length; i += 1) {
+    removeBtns[i].addEventListener('click', () => {
+      removeBook(i);
+    });
+  }
+};
+
 const appendBook = (book) => {
-  let bookElement = document.createElement('div');
-  let titleSpan = document.createElement('span');
+  const bookElement = document.createElement('div');
+  bookElement.classList.add('book-element-wrapper');
+  const titleSpan = document.createElement('span');
   titleSpan.innerText = book.title;
-  let br = document.createElement('br');
-  let authorSpan = document.createElement('span');
+  const br = document.createElement('br');
+  const authorSpan = document.createElement('span');
   authorSpan.innerText = book.author;
-  let br2 = document.createElement('br');
-  let rmvBtn = document.createElement('button');
+  const br2 = document.createElement('br');
+  const rmvBtn = document.createElement('button');
   rmvBtn.classList.add('remove-button');
   rmvBtn.innerText = 'remove';
-  let hr = document.createElement('hr');
+  const hr = document.createElement('hr');
 
   bookElement.appendChild(titleSpan);
   bookElement.appendChild(br);
@@ -30,6 +46,7 @@ const appendBook = (book) => {
   bookElement.appendChild(rmvBtn);
   bookElement.appendChild(hr);
   bookList.appendChild(bookElement);
+  addEventToRemoveButtons();
 };
 
 books.forEach((book) => {
@@ -45,34 +62,4 @@ addBtn.addEventListener('click', () => {
   const newBook = new Book(title.value, author.value);
   books.push(newBook);
   appendBook(newBook);
-  //   let bookElement = document.createElement('div');
-  //   console.log(newBook.title);
-  //   let titleSpan = document.createElement('span');
-  //   titleSpan.innerText = newBook.title;
-  //   let br = document.createElement('br');
-  //   let authorSpan = document.createElement('span');
-  //   authorSpan.innerText = newBook.author;
-  //   let br2 = document.createElement('br');
-  //   let rmvBtn = document.createElement('button');
-  //   rmvBtn.classList.add('remove-button');
-  //   rmvBtn.innerText = 'remove';
-  //   let hr = document.createElement('hr');
-
-  //   bookElement.appendChild(titleSpan);
-  //   bookElement.appendChild(br);
-  //   bookElement.appendChild(authorSpan);
-  //   bookElement.appendChild(br2);
-  //   bookElement.appendChild(rmvBtn);
-  //   bookElement.appendChild(hr);
-  //   bookList.appendChild(bookElement);
 });
-
-const removeBook = (i) => {
-  books = books.filter((a) => a !== books[i]);
-};
-
-for (let i = 0; i < removeBtns.length; i += 1) {
-  removeBtns[i].addEventListener('click', () => {
-    removeBook(i);
-  });
-}
