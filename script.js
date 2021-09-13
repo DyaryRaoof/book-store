0a3573c4c814c7a852b8bbbb8c6d2dfa1127cbea
Dyary Raoof Bayz <dyary2020@gmail.com>
2021-09-13 23:30:50 +0300

    Add remove book functionality

--- script.js
+++ script.js
@@ -3,18 +3,30 @@ const book1 = {
   author: 'author1',
 };
 
-const books = [book1];
+let books = [book1];
 
 const form = document.querySelector('#form');
 const title = form.elements[0];
 const author = form.elements[1];
 const addBtn = document.querySelector('#addBtn');
-addBtn.addEventListener('click', () => {
-  let newBook = new Book(title.value, author.value);
-  books.push(newBook);
-});
+const removeBtns = document.querySelectorAll('.remove-button');
 
 function Book(title, author) {
   this.title = title;
   this.author = author;
+}
+
+addBtn.addEventListener('click', () => {
+  const newBook = new Book(title.value, author.value);
+  books.push(newBook);
+});
+
+const removeBook = (i) => {
+  books = books.filter((a) => a.title !== books[i].title);
+};
+
+for (let i = 0; i < removeBtns.length; i += 1) {
+  removeBtns[i].addEventListener('click', () => {
+    removeBook(i);
+  });
 }
